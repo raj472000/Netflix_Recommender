3f400fa8818590db58954dd45b6907b67a75ed10
raj472000 <sanapalarajesh04@gmail.com>
2026-04-28 23:07:31 +0530

    s

--- app/main.py
+++ app/main.py
@@ -7,7 +7,7 @@ from app.middleware.request_middleware import RequestMiddleware
 
 app = FastAPI(
     title="Advanced Recommender V2",
-    version="2.0.0",
+    version="2.0.1",
 )
 
 #app.add_middleware(RequestMiddleware)
